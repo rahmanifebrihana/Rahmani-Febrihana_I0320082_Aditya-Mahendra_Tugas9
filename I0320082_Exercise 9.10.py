d7133d61d72f1da2560dfc77824d051de4c56c42
rahmani febrihana <rfebrihana@gmail.com>
2021-05-25 09:36:03 -0800

    Pengerjaan Exercise

---
+++ I0320082_Exercise 9.10.py
@@ -0,0 +1,10 @@
+# nilai awal (sebelum dibalik)
+import array
+A = [100, -700, 300, 400, 800]
+print("Nilai awal sebelum dibalik: ", A)
+
+# membalik urutan elemen array
+A.reverse()
+
+# nilai akhir (setelah dibalik)
+print("Nilai akhir setelah dibalik: ", A)
---
+++ I0320082_Exercise 9.8.py
@@ -0,0 +1,11 @@
+# mengonversi list ke dalam array.array
+li = [10, 20, 30, 40, 50]
+
+import array
+C = array.array('i')
+C.fromlist(li)
+type(C)
+
+for nilai in C:
+    print("%d " % nilai, end='')
+
